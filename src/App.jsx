a02f28b5e8e76ec4f9faf3ff09ce0fbe45077f03
TerranceRivers <109315955+TerranceRivers@users.noreply.github.com>
2023-07-20 15:46:04 -0400

    Yes or No

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import YesorNoComponent from "./components/YesOrNo";
 function App() {
-
   return (
     <>
+      <RadioButtonComponent />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
