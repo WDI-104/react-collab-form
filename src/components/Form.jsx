0c5d656eb4838ab23d5c6d56b65cfd9a4a51c449
Juan <juan.giana8589@gmail.com>
2023-07-20 21:33:31 -0500

    added skinWalkers question

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,12 +1,15 @@
-import React, {useState}from 'react'
+import React, { useState } from 'react';
 
 const Form = () => {
-    const [answer1, setAnswer1] = useState("");
-    const [answer2, setAnswer2] = useState("");
-    const [answer3, setAnswer3] = useState("");
-    const [answer4, setAnswer4] = useState("");
+  // State to hold the answers
+  const [answers, setAnswers] = useState({
+    question1: '',
+    question2: '',
+    question3: '',
+    skinWalkers: '', // SkinWalkers question
+  });
 
-    // State to store the submitted answers as a list
+  // State to store the submitted answers as a list
   const [submittedAnswers, setSubmittedAnswers] = useState([]);
 
   // Handle form submission
@@ -16,32 +19,103 @@ const Form = () => {
     // Add the current answers to the submitted answers list
     setSubmittedAnswers([
       ...submittedAnswers,
-      { question: 'Question 1', answer: answer1 },
-      { question: 'Question 2', answer: answer2 },
-      { question: 'Question 3', answer: answer3 },
-      { question: 'Question 4', answer: answer4 },
+      { question: 'Question 1 Goes Here', answer: answers.question1 },
+      { question: 'Question 2 Goes Here', answer: answers.question2 },
+      { question: 'Question 3 Goes Here', answer: answers.question3 },
+      { question: 'Do you believe in SkinWalkers?', answer: answers.skinWalkers },
     ]);
 
     // Clear the form after submission
-    setAnswer1('');
-    setAnswer2('');
-    setAnswer3('');
-    setAnswer4('');
+    setAnswers({
+      question1: '',
+      question2: '',
+      question3: '',
+      skinWalkers: '',
+    });
+  };
+
+  // Handle input change for questions
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setAnswers((prevAnswers) => ({
+      ...prevAnswers,
+      [name]: value,
+    }));
   };
 
   return (
     <div>
-        <h1>Form</h1>
-        <form onSubmit={handleOnSubmit}>
-            {/* Input fields for answers */}
-        {/* <label> example
-          Question 1:
-          <input type="text" value={answer1} onChange={(e) => setAnswer1(e.target.value)} />
-        </label> */}
-        
-        <button>Submit</button>
-        </form>
-        {/* Display submitted answers as a list */}
+      <h1>Form</h1>
+      <form onSubmit={handleOnSubmit}>
+        {/* Input fields for answers */}
+        <div>
+          <label>
+            Question 1 Goes Here:
+            <input
+              type="text"
+              name="question1"
+              value={answers.question1}
+              onChange={handleInputChange}
+            />
+          </label>
+        </div>
+        <br />
+        <div>
+          <label>
+            Question 2 Goes Here:
+            <input
+              type="text"
+              name="question2"
+              value={answers.question2}
+              onChange={handleInputChange}
+            />
+          </label>
+        </div>
+        <br />
+        <div>
+          <label>
+            Question 3 Goes Here:
+            <input
+              type="text"
+              name="question3"
+              value={answers.question3}
+              onChange={handleInputChange}
+            />
+          </label>
+        </div>
+        <br />
+
+        {/* SkinWalkers question */}
+        <div>
+          <h1>Do you believe in SkinWalkers?</h1>
+          <label>
+            <input
+              type="radio"
+              name="skinWalkers"
+              value="yes"
+              checked={answers.skinWalkers === 'yes'}
+              onChange={handleInputChange}
+            />
+            Yes
+          </label>
+          <label>
+            <input
+              type="radio"
+              name="skinWalkers"
+              value="no"
+              checked={answers.skinWalkers === 'no'}
+              onChange={handleInputChange}
+            />
+            No
+          </label>
+        </div>
+        <br />
+
+        {/* Submit button */}
+        <button type="submit">Submit</button>
+      </form>
+
+      {/* Display submitted answers as a list */}
       <div>
         <h2>Submitted Answers:</h2>
         <ul>
@@ -53,9 +127,8 @@ const Form = () => {
           ))}
         </ul>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Form
+export default Form;
